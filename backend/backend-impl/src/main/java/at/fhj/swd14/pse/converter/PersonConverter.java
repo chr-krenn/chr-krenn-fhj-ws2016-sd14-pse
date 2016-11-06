23eaa4be5b0e59bf9624bad6faa8c8ceb8b4c324
Patrick Kainz <patrick.kainz@edu.fh-joanneum.at>
2016-11-06 19:22:54 +0100

    resolved/answered fixmes

--- backend/backend-impl/src/main/java/at/fhj/swd14/pse/converter/PersonConverter.java
+++ backend/backend-impl/src/main/java/at/fhj/swd14/pse/converter/PersonConverter.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import at.fhj.swd14.pse.person.*;
-import at.fhj.swd14.pse.repository.PersonRepository;
-import at.fhj.swd14.pse.repository.PersonStatusRepository;
 
 public class PersonConverter {
 
@@ -55,57 +53,6 @@ public class PersonConverter {
         return dto;
     }
 
-    //FIXME @pkainz converter shouldn't rely on repository injection ... the checks performed with the repositories should be done in the service instead!
-    // try to use #convert(PersonDto) instead
-    public static Person convert(PersonDto dto, PersonStatusRepository statusRepo, PersonRepository personRepo) {
-        if (dto == null) {
-            return null;
-        }
-
-        Person oldPerson = personRepo.find(dto.getId());
-        if (oldPerson == null)
-            throw new IllegalArgumentException("Person not found " + dto.getId());
-
-        Person person = new Person(dto.getId(), UserConverter.convert(dto.getUser()));
-        person.setAddress(dto.getAddress());
-        person.setDepartment(DepartmentConverter.convert(dto.getDepartment()));
-        person.setFirstname(dto.getFirstname());
-        person.setImageUrl(dto.getImageUrl());
-        person.setLastname(dto.getLastname());
-        person.setPlace(dto.getPlace());
-        person.setStatus(statusRepo.findByName(dto.getStatus()));
-
-        person.setAdditionalMails(new LinkedList<>());
-        for (String mail : dto.getAdditionalMails()) {
-            Mailaddress address = new Mailaddress();
-            address.setValue(mail);
-            person.getAdditionalMails().add(address);
-        }
-
-        person.setHobbies(new LinkedList<>());
-        for (String hobby : dto.getHobbies()) {
-            Hobby newhobby = new Hobby();
-            newhobby.setValue(hobby);
-            person.getHobbies().add(newhobby);
-        }
-
-        person.setKnowledges(new LinkedList<>());
-        for (String knowledge : dto.getKnowledges()) {
-            Knowledge newknowledge = new Knowledge();
-            newknowledge.setValue(knowledge);
-            person.getKnowledges().add(newknowledge);
-        }
-
-        person.setNumbers(new LinkedList<>());
-        for (String number : dto.getPhonenumbers()) {
-            Phonenumber newnumber = new Phonenumber();
-            newnumber.setValue(number);
-            person.getNumbers().add(newnumber);
-        }
-
-        return person;
-    }
-
     public static Person convert(PersonDto dto) {
         if (dto == null) {
             return null;
@@ -118,12 +65,11 @@ public class PersonConverter {
         person.setImageUrl(dto.getImageUrl());
         person.setLastname(dto.getLastname());
         person.setPlace(dto.getPlace());
-        //FIXME @pkainz this would obviously lead to a status duplication
-        // why does the status has to have a dedicated id column? Isn't the status' name unique enough?
-        // Note: This is the version without the repository access from above method
         person.setStatus(new Status(dto.getStatus()));
 
-        //FIXME @pkainz why converting these additional infos manually? why no separate converter?
+        //@pkainz why converting these additional infos manually? why no separate converter?
+        //Because they are only used in the context of persons, so why create another class for them
+        //if noone except this class is going to use it.
         person.setAdditionalMails(new LinkedList<>());
         for (String mail : dto.getAdditionalMails()) {
             Mailaddress address = new Mailaddress();
@@ -162,14 +108,14 @@ public class PersonConverter {
         return persons.stream().map(PersonConverter::convert).collect(Collectors.toList());
     }
 
-    public static Collection<Person> convertToDoList(Collection<PersonDto> persons, PersonStatusRepository statusRepo, PersonRepository personRepo) {
+    public static Collection<Person> convertToDoList(Collection<PersonDto> persons) {
         if (persons == null) {
             return null;
         }
         List<Person> newpersons = new LinkedList<>();
 
         for (PersonDto dto : persons) {
-            newpersons.add(convert(dto, statusRepo, personRepo));
+            newpersons.add(convert(dto));
         }
 
         return newpersons;

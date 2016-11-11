02cbbefa0d238b67b330aa9d50f76be4bed93b70
Neuherz Alexander <alexander.neuherz@edu.fh-joanneum.at>
2016-11-11 15:31:24 +0100

    bugfix imports Tag class (jenkins failed to build)

--- backend/backend-impl/src/main/java/at/fhj/swd14/pse/tag/Tag.java
+++ backend/backend-impl/src/main/java/at/fhj/swd14/pse/tag/Tag.java
@@ -1,12 +1,6 @@
 package at.fhj.swd14.pse.tag;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity

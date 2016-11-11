6ae75c0d925621fcdb2fdc3fa43340595d274592
Georg Gölles <georg.goelleshaas@edu.fh-joanneum.at>
2016-11-11 17:45:26 +0100

    added tag tests

---
+++ backend/backend-impl/src/test/java/at/fhj/swd14/pse/tag/TagServiceImplTest.java
@@ -0,0 +1,79 @@
+package at.fhj.swd14.pse.tag;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.naming.NamingException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import at.fhj.swd14.pse.converter.TagConverter;
+import at.fhj.swd14.pse.repository.TagRepository;
+
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class TagServiceImplTest {
+	@InjectMocks
+	private TagServiceImpl service;
+	
+	@Mock
+	private TagRepository tagRepo;
+	
+	private List<Tag> tags;
+
+	@Before
+	public void setup() throws NamingException
+	{
+		tags = new ArrayList<Tag>();
+		Tag tag = new Tag(1L);
+		tag.setName("testtag1");
+		
+		tags.add(tag);
+        
+	}
+
+	@Test
+	public void testFind()
+	{
+		Mockito.when(tagRepo.find(1L)).thenReturn(tags.get(0));
+		TagDto t = service.find(1);
+
+		TagDtoTester.assertEquals(TagConverter.convert(tags.get(0)), t);
+
+	}
+	
+	@Test
+	public void testFindByName()
+	{
+		Map<String, Object> parameter = new HashMap<>();
+		parameter.put("tagName", "testtag1");
+		
+		Mockito.when(tagRepo.executeNamedQuery("Tag.findByName", parameter)).thenReturn(tags);
+		TagDto t = service.findByName("testtag1");
+
+		TagDtoTester.assertEquals(TagConverter.convert(tags.get(0)), t);
+
+	}
+	
+	@Test
+	public void testFindAll()
+	{
+		
+		Mockito.when(tagRepo.findAll()).thenReturn(tags);
+		List<TagDto> l = service.findAll();
+
+		Assert.assertEquals(1, l.size());
+		TagDtoTester.assertEquals(TagConverter.convert(tags.get(0)), l.get(0));
+
+	}
+}
---
+++ backend/backend-testing-commons/src/main/java/at/fhj/swd14/pse/tag/TagDtoTester.java
@@ -0,0 +1,12 @@
+package at.fhj.swd14.pse.tag;
+
+import org.junit.Assert;
+
+public class TagDtoTester {
+
+	public static void assertEquals(TagDto expected, TagDto actual) {
+		Assert.assertEquals(expected.getId(), actual.getId());
+		Assert.assertEquals(expected.getName(), actual.getName());
+	}
+
+}

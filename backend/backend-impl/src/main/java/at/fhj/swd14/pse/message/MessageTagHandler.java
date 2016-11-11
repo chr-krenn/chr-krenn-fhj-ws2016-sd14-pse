d0a7be7ddae70e00248a91a88b68fdeab3cd5b7b
Richard Raumberger <richard.raumberger@edu.fh-joanneum.at>
2016-11-11 16:06:39 +0100

    remove unnecessary ejb annotation on TagServiceImpl

--- backend/backend-impl/src/main/java/at/fhj/swd14/pse/message/MessageTagHandler.java
+++ backend/backend-impl/src/main/java/at/fhj/swd14/pse/message/MessageTagHandler.java
@@ -12,59 +12,59 @@ import at.fhj.swd14.pse.converter.MessageConverter;
 import at.fhj.swd14.pse.converter.TagConverter;
 import at.fhj.swd14.pse.tag.Tag;
 import at.fhj.swd14.pse.tag.TagDto;
-import at.fhj.swd14.pse.tag.TagServiceImpl;
+import at.fhj.swd14.pse.tag.TagService;
 
 public class MessageTagHandler {
 
-	@EJB
-	private TagServiceImpl tagService;
-	
-	public void handleTags(MessageDto dto){
-		if (dto != null) {
-			
-			Message message = MessageConverter.convert(dto);
-			
-			Set<String> tagSet = new HashSet<String>();
-			
-			// search for tags in title
-			tagSet.addAll(findTags(message.getTitle()));
-			// search for tags in context
-			tagSet.addAll(findTags(message.getContent()));
-			
-			TagDto tagDto;
-			
-			for (String tag : tagSet) {
-				
-				tagDto = tagService.findByName(tag);
-				
-				if (tagDto == null) {
-					Tag t = new Tag();
-					t.setName(tag);
-					tagDto = TagConverter.convert(t);
-					tagService.save(tagDto);
-				}
-				// add Tag to Message
-				//message.addTag(tagDto);
-			}
-			dto = MessageConverter.convert(message);
+    @EJB
+    private TagService tagService;
 
-		}
-		
-	}
-	
-	private Set<String> findTags(String text) {
-		Set<String> tagSet = new HashSet<String>();
-		
-		Pattern p = Pattern.compile("(\\s|\\A)#(\\w+)");
+    public void handleTags(MessageDto dto) {
+        if (dto != null) {
 
-		Matcher matcher = p.matcher(text);
+            Message message = MessageConverter.convert(dto);
 
-		if (matcher.find()) {
-			for (int i = 0; i < matcher.groupCount(); i++) {
-				tagSet.add(matcher.group(i).toLowerCase());
-			}
-		}
-		
-		return tagSet;
-	}
+            Set<String> tagSet = new HashSet<>();
+
+            // search for tags in title
+            tagSet.addAll(findTags(message.getTitle()));
+            // search for tags in context
+            tagSet.addAll(findTags(message.getContent()));
+
+            TagDto tagDto;
+
+            for (String tag : tagSet) {
+
+                tagDto = tagService.findByName(tag);
+
+                if (tagDto == null) {
+                    Tag t = new Tag();
+                    t.setName(tag);
+                    tagDto = TagConverter.convert(t);
+                    tagService.save(tagDto);
+                }
+                // add Tag to Message
+                //message.addTag(tagDto);
+            }
+            dto = MessageConverter.convert(message);
+
+        }
+
+    }
+
+    private Set<String> findTags(String text) {
+        Set<String> tagSet = new HashSet<>();
+
+        Pattern p = Pattern.compile("(\\s|\\A)#(\\w+)");
+
+        Matcher matcher = p.matcher(text);
+
+        if (matcher.find()) {
+            for (int i = 0; i < matcher.groupCount(); i++) {
+                tagSet.add(matcher.group(i).toLowerCase());
+            }
+        }
+
+        return tagSet;
+    }
 }
--- backend/backend-impl/src/main/java/at/fhj/swd14/pse/tag/TagServiceImpl.java
+++ backend/backend-impl/src/main/java/at/fhj/swd14/pse/tag/TagServiceImpl.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.Map;
 
 import javax.ejb.EJB;
-import javax.ejb.Local;
-import javax.ejb.Singleton;
 import javax.ejb.Stateless;
 
 import at.fhj.swd14.pse.converter.TagConverter;
@@ -15,39 +13,35 @@ import at.fhj.swd14.pse.repository.TagRepository;
 
 //Todo: implement methods
 @Stateless
-@Local
-@Singleton
-public class TagServiceImpl implements TagService{
+public class TagServiceImpl implements TagService {
 
-	@EJB
-	private TagRepository tagRepository;
-    
-	 @Override
+    @EJB
+    private TagRepository tagRepository;
+
+    @Override
     public long save(TagDto tag) {
-		 Tag doTag = TagConverter.convert(tag);
-	     tagRepository.save(doTag);
-	     return doTag.getId();
+        Tag doTag = TagConverter.convert(tag);
+        tagRepository.save(doTag);
+        return doTag.getId();
     }
 
     @Override
     public TagDto find(long id) {
-    	return TagConverter.convert(tagRepository.find(id));
+        return TagConverter.convert(tagRepository.find(id));
     }
 
     @Override
     public TagDto findByName(String name) {
-    	Map<String, Object> parameter = new HashMap<>();
-		parameter.put("tagName", name);
-		List<TagDto> tagList = new ArrayList<TagDto>();
-		
-		tagList = executeNamedQuery("Tag.findByName", parameter);
-		if (tagList.size() > 0)
-			return tagList.get(0);
-		else
-			return null;
+        Map<String, Object> parameter = new HashMap<>();
+        parameter.put("tagName", name);
+        List<TagDto> tagList = executeNamedQuery("Tag.findByName", parameter);
+        if (tagList.size() > 0)
+            return tagList.get(0);
+        else
+            return null;
     }
-    
-    private List<TagDto> executeNamedQuery(String name, Map<String, Object> parameter){
-		return new ArrayList<>(TagConverter.convertToDtoList(tagRepository.executeNamedQuery(name, parameter)));
-	}
+
+    private List<TagDto> executeNamedQuery(String name, Map<String, Object> parameter) {
+        return new ArrayList<>(TagConverter.convertToDtoList(tagRepository.executeNamedQuery(name, parameter)));
+    }
 }

ed4e4af780c4abebbda6a65136f87028661de899
Richard Raumberger <richard.raumberger@edu.fh-joanneum.at>
2016-11-11 15:23:34 +0100

    extract seeddata from testdata; refactor sql; fix sql-file nameing

---
+++ database/src/main/resources/sql/seeddata-dml.sql
@@ -0,0 +1,8 @@
+#department
+INSERT INTO department (name) VALUES ("IT");
+INSERT INTO department (name) VALUES ("Accounting");
+
+#person_status
+INSERT INTO person_status (name) VALUES ('online');
+INSERT INTO person_status (name) VALUES ('offline');
+INSERT INTO person_status (name) VALUES ('abwesend');

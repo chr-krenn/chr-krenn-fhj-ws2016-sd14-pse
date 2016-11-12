5513f075de67bee86b863ae4d7bf44d99053bdf4
pellumbi14 <alb.pellumbi@edu.fh-joanneum.at>
2016-11-12 18:24:35 +0100

    recomment @stateful und @viewscoped

--- frontend/frontend-impl/src/main/java/at/fhj/swd14/pse/message/MessageStreamBean.java
+++ frontend/frontend-impl/src/main/java/at/fhj/swd14/pse/message/MessageStreamBean.java
@@ -1,25 +1,29 @@
 package at.fhj.swd14.pse.message;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 //TODO: Change CommunityDtoStub to CommunityDto in this file
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Stateful;
-import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import java.io.Serializable;
-import java.util.*;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import at.fhj.swd14.pse.comment.CommentDto;
 import at.fhj.swd14.pse.community.CommunityDtoStub;
-import at.fhj.swd14.pse.user.UserDto;
 
 @Named
-//@Stateful
-//@ViewScoped
-public class MessageStreamBean implements Serializable {
+@Stateful
+@ViewScoped
+public class MessageStreamBean implements Serializable
+{
 	// ---------- Member including Get/Set ------------
 	private static final long serialVersionUID = 1L;
 
@@ -41,7 +45,8 @@ public class MessageStreamBean implements Serializable {
 	 *
 	 * @return The current communities DTO
 	 */
-	public CommunityDtoStub getCurrentCommunity() {
+	public CommunityDtoStub getCurrentCommunity()
+	{
 		return this.currentCommunity;
 	}
 
@@ -51,7 +56,8 @@ public class MessageStreamBean implements Serializable {
 	 * @param currentCommunity
 	 *            The community to set as current community
 	 */
-	public void setCurrentCommunity(CommunityDtoStub currentCommunity) {
+	public void setCurrentCommunity(CommunityDtoStub currentCommunity)
+	{
 		this.currentCommunity = currentCommunity;
 	}
 
@@ -65,7 +71,8 @@ public class MessageStreamBean implements Serializable {
 	 *
 	 * @return The available communities
 	 */
-	public List<CommunityDtoStub> getAvailableCommunities() {
+	public List<CommunityDtoStub> getAvailableCommunities()
+	{
 		return availableCommunities;
 	}
 
@@ -75,7 +82,8 @@ public class MessageStreamBean implements Serializable {
 	 * @param availableCommunities
 	 *            The available communities to set
 	 */
-	public void setAvailableCommunities(List<CommunityDtoStub> availableCommunities) {
+	public void setAvailableCommunities(List<CommunityDtoStub> availableCommunities)
+	{
 		this.availableCommunities = availableCommunities;
 	}
 
@@ -89,7 +97,8 @@ public class MessageStreamBean implements Serializable {
 	 *
 	 * @return
 	 */
-	public Map<Long, MessageDto> getMessages() {
+	public Map<Long, MessageDto> getMessages()
+	{
 		return this.messages;
 	}
 
@@ -99,7 +108,8 @@ public class MessageStreamBean implements Serializable {
 	 * @param messages
 	 *            the messages to display in the view
 	 */
-	public void setMessages(Map<Long, MessageDto> messages) {
+	public void setMessages(Map<Long, MessageDto> messages)
+	{
 		this.messages = messages;
 	}
 
@@ -113,7 +123,8 @@ public class MessageStreamBean implements Serializable {
 	 *
 	 * @return
 	 */
-	public long getCurrentUserId() {
+	public long getCurrentUserId()
+	{
 		return this.currentUserId;
 	}
 
@@ -123,30 +134,34 @@ public class MessageStreamBean implements Serializable {
 	 * @param id
 	 *            the Id of the currently logged-in User
 	 */
-	public void setCurrentUserId(long id) {
+	public void setCurrentUserId(long id)
+	{
 		this.currentUserId = id;
 	}
-	// ---------- Constructor  ------------
-	
+	// ---------- Constructor ------------
+
 	/*
 	 *
 	*/
-	public MessageStreamBean() {
-    	LOGGER.debug("Create: " + MessageStreamBean.class.getSimpleName());
+	public MessageStreamBean()
+	{
+		LOGGER.debug("Create: " + MessageStreamBean.class.getSimpleName());
 	}
-	
+
 	/**
 	 * Initialises the bean for the view
 	 */
 	@PostConstruct
-	public void init() {
+	public void init()
+	{
 		LOGGER.debug("Initialising the MessageStreamBean");
 		this.initAvailableCommunities();
-		
-//		Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-//	    int userId = Integer.parseInt(params.get("userId"));
-//		setCurrentUserId(userId);
-	    this.setCurrentUserId(1L);
+
+		// Map<String,String> params =
+		// FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		// int userId = Integer.parseInt(params.get("userId"));
+		// setCurrentUserId(userId);
+		this.setCurrentUserId(1L);
 	}
 
 	// ---------- Public Methods ------------
@@ -156,7 +171,8 @@ public class MessageStreamBean implements Serializable {
 	 *
 	 * @return
 	 */
-	public Map<Long, MessageDto> getAllMessages() {
+	public Map<Long, MessageDto> getAllMessages()
+	{
 		LOGGER.debug("Getting all Messages");
 		return mapMessages(this.messageService.findUserRelated(this.getCurrentUserId()));
 	}
@@ -168,9 +184,11 @@ public class MessageStreamBean implements Serializable {
 	 *            The ID of the message to get the comments for
 	 * @return A list of the messages comments
 	 */
-	public List<CommentDto> getComments(Long messageId) {
+	public List<CommentDto> getComments(Long messageId)
+	{
 		MessageDto message = messages.get(messageId);
-		if (message != null) {
+		if (message != null)
+		{
 			return message.getChilds();
 		}
 
@@ -185,7 +203,8 @@ public class MessageStreamBean implements Serializable {
 	 * @param text
 	 *            the text for the new comment
 	 */
-	public void createComment(Long messageId, String text) {
+	public void createComment(Long messageId, String text)
+	{
 		LOGGER.debug("createComment: " + messageId + " - " + text);
 		// TODO: Implement
 	}
@@ -196,7 +215,8 @@ public class MessageStreamBean implements Serializable {
 	 * @param id
 	 *            The id of the message to like
 	 */
-	public void likeMessage(Long id) {
+	public void likeMessage(Long id)
+	{
 		LOGGER.debug("likeMessage: " + id);
 		// TODO: Implement
 	}
@@ -207,7 +227,8 @@ public class MessageStreamBean implements Serializable {
 	 * @param id
 	 *            The id of the comment to like
 	 */
-	public void likeComment(Long id) {
+	public void likeComment(Long id)
+	{
 		LOGGER.debug("likeComment: " + id);
 		// TODO: Implement
 	}
@@ -215,20 +236,28 @@ public class MessageStreamBean implements Serializable {
 	/**
 	 * Changes the currentCommunity to the selected value (called by Server)
 	 */
-	public void onCommunityChange() {
+	public void onCommunityChange()
+	{
 		LOGGER.debug("MessageStreamBean Community Changed: " + currentCommunity);
-		if (this.currentCommunity.getId() <= -3) { // All Messages
+		if (this.currentCommunity.getId() <= -3)
+		{ // All Messages
 			this.messages = this.getAllMessages();
-		} else if (this.currentCommunity.getId() == -2) { // Private Messages
+		} else if (this.currentCommunity.getId() == -2)
+		{ // Private Messages
 			this.messages = this.getPrivateMessages();
-		} else if (this.currentCommunity.getId() == -1) { // Global Messages
+		} else if (this.currentCommunity.getId() == -1)
+		{ // Global Messages
 			this.messages = this.getGlobalMessages();
-		} else if (this.currentCommunity.getId() == 0) { // All Messages
+		} else if (this.currentCommunity.getId() == 0)
+		{ // All Messages
 			this.messages = this.getAllMessages();
-		} else if (this.currentCommunity.getId() > 0){ // Specific Community Message
+		} else if (this.currentCommunity.getId() > 0)
+		{ // Specific Community Message
 			this.messages = this.getCommunityMessages(this.currentCommunity.getId());
-		} else {
-			this.messages = this.getAllMessages(); // Default -> here should be some ErrorHandling
+		} else
+		{
+			this.messages = this.getAllMessages(); // Default -> here should be
+													// some ErrorHandling
 		}
 	}
 
@@ -237,7 +266,8 @@ public class MessageStreamBean implements Serializable {
 	/**
 	 * Initialises the availableCommunities (called by Server)
 	 */
-	private void initAvailableCommunities() {
+	private void initAvailableCommunities()
+	{
 		LOGGER.debug("Initialising the Communities for the Selectbox");
 		// TODO: Implement in CommunityService or MessageService with correct
 		// return type.
@@ -248,31 +278,32 @@ public class MessageStreamBean implements Serializable {
 		// ID -1 => Name "Globale"
 		// this.availableCommunities = communityService.findAllUserRelated
 		this.availableCommunities = new ArrayList<CommunityDtoStub>();
-		
+
 		CommunityDtoStub all = new CommunityDtoStub();
 		all.setId(-3L);
 		all.setName("ALLE");
 		this.availableCommunities.add(all);
-		
+
 		CommunityDtoStub privateC = new CommunityDtoStub();
 		privateC.setId(-2L);
 		privateC.setName("Private");
 		this.availableCommunities.add(privateC);
-		
+
 		CommunityDtoStub global = new CommunityDtoStub();
 		global.setId(-1L);
 		global.setName("Globale");
 		this.availableCommunities.add(global);
-		
-		for (int i = 1; i < 4; i++) {
+
+		for (int i = 1; i < 4; i++)
+		{
 			CommunityDtoStub c = new CommunityDtoStub();
 			c.setId((long) i);
 			c.setName("Community_" + c.getId());
 			this.availableCommunities.add(c);
 		}
-		
+
 		this.setCurrentCommunity(availableCommunities.get(0));
-		
+
 		onCommunityChange();
 	}
 
@@ -283,7 +314,8 @@ public class MessageStreamBean implements Serializable {
 	 *            The id of the community
 	 * @return A map of Messages
 	 */
-	private Map<Long, MessageDto> getCommunityMessages(Long id) {
+	private Map<Long, MessageDto> getCommunityMessages(Long id)
+	{
 		return mapMessages(this.messageService.findByCommunityId(id));
 	}
 
@@ -292,7 +324,8 @@ public class MessageStreamBean implements Serializable {
 	 *
 	 * @return A map of Messages
 	 */
-	private Map<Long, MessageDto> getGlobalMessages() {
+	private Map<Long, MessageDto> getGlobalMessages()
+	{
 		return mapMessages(this.messageService.findGlobalMesssages());
 	}
 
@@ -301,23 +334,24 @@ public class MessageStreamBean implements Serializable {
 	 *
 	 * @return A map of Messages
 	 */
-	private Map<Long, MessageDto> getPrivateMessages() {
+	private Map<Long, MessageDto> getPrivateMessages()
+	{
 		return mapMessages(this.messageService.findUsersPrivateMessages(this.getCurrentUserId()));
 	}
-	
+
 	/**
-	 * Maps a List of Messages
-	 * Key: Id of Message
-	 * Value: Message
+	 * Maps a List of Messages Key: Id of Message Value: Message
 	 * 
 	 * @param messageList
-	 * 				List of messages which should be mapped
+	 *            List of messages which should be mapped
 	 * 
 	 * @return A map of Messages
 	 */
-	private Map<Long, MessageDto> mapMessages(List<MessageDto> messageList){
+	private Map<Long, MessageDto> mapMessages(List<MessageDto> messageList)
+	{
 		Map<Long, MessageDto> mappedMessages = new HashMap<Long, MessageDto>();
-		for (MessageDto message : messageList) {
+		for (MessageDto message : messageList)
+		{
 			mappedMessages.put(message.getId(), message);
 		}
 		return mappedMessages;

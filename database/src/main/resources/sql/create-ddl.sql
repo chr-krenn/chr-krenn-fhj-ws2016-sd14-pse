605ee051d4b0f81e8ac40458c66127d6e46157f3
Patrick Kainz <patrick.kainz@edu.fh-joanneum.at>
2016-12-03 10:06:38 +0100

    mariadb doesn't seem to like tabs very much

--- database/src/main/resources/sql/create-ddl.sql
+++ database/src/main/resources/sql/create-ddl.sql
@@ -198,15 +198,15 @@ CREATE TABLE IF NOT EXISTS message_tag
 #create join table for likes for messages
 CREATE TABLE IF NOT EXISTS like_for_message
 (
-  message_id	BIGINT	NOT NULL REFERENCES message(id),
-  user_id		BIGINT 	NOT NULL REFERENCES `user`(id)
+  message_id    BIGINT  NOT NULL REFERENCES message(id),
+  user_id       BIGINT  NOT NULL REFERENCES `user`(id)
 )
   ENGINE = INNODB;
   
 #create join table for likes for comments
 CREATE TABLE IF NOT EXISTS like_for_comment
 (
-  comment_id	BIGINT NOT NULL REFERENCES comment(id),
-  user_id		BIGINT NOT NULL REFERENCES `user`(id)
+  comment_id    BIGINT NOT NULL REFERENCES comment(id),
+  user_id       BIGINT NOT NULL REFERENCES `user`(id)
 )
   ENGINE = INNODB;

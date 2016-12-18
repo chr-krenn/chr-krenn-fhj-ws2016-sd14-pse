9fd8a277f8a317d9b25eeb457464f7b2771cc0d3
Richard Raumberger <richard.raumberger@edu.fh-joanneum.at>
2016-12-18 19:47:29 +0100

    fix stackoverflow when converting messages with comments

--- backend/backend-impl/src/main/java/at/fhj/swd14/pse/converter/MessageConverter.java
+++ backend/backend-impl/src/main/java/at/fhj/swd14/pse/converter/MessageConverter.java
@@ -7,18 +7,17 @@ import java.util.stream.Collectors;
 import at.fhj.swd14.pse.message.Message;
 import at.fhj.swd14.pse.message.MessageDto;
 
-public class MessageConverter {
+public final class MessageConverter {
 
-	
-	public MessageConverter(){
-		
-	}
-	
-	public static MessageDto convert(Message message) {
+
+    private MessageConverter() {
+    }
+
+    public static MessageDto convert(Message message) {
         if (message == null) {
             return null;
         }
-        
+
         MessageDto dto = new MessageDto(message.getId());
         dto.setAuthor(UserConverter.convert(message.getAuthor()));
         dto.setRecipient(UserConverter.convert(message.getRecipient()));
@@ -27,7 +26,11 @@ public class MessageConverter {
         dto.setContent(message.getContent());
         dto.setCreated(message.getCreated());
         dto.setModified(message.getModified());
+
+        //Reset the parent message to avoid a stackoverflow by infinitely converting the message and it's children
+        message.getChilds().forEach(child -> child.setParentMessage(null));
         dto.setChilds(new ArrayList<>(CommentConverter.convertToDtoList(message.getChilds())));
+        dto.getChilds().forEach(child -> child.setParentMessage(dto));
         return dto;
     }
 
@@ -41,7 +44,11 @@ public class MessageConverter {
         message.setCommunity(CommunityConverter.convert(dto.getCommunity()));
         message.setTitle(dto.getTitle());
         message.setContent(dto.getContent());
+
+        //Reset the parent message to avoid a stackoverflow by infinitely converting the message and it's children
+        dto.getChilds().forEach(child -> child.setParentMessage(null));
         message.setChilds(new ArrayList<>(CommentConverter.convertToDoList(dto.getChilds())));
+        message.getChilds().forEach(child -> child.setParentMessage(message));
         //no need to set creation or updating timestamp --> automatically set on DB-interaction
         return message;
     }

24ac60d83d41701ad1d8beffab5c61efa30f0540
Patrick Kainz <patrick.kainz@edu.fh-joanneum.at>
2016-12-18 20:44:18 +0100

    fixed some more sonarqube issues

--- backend/backend-testing-commons/src/main/java/at/fhj/swd14/pse/person/AbstractPersonInformationDtoTester.java
+++ backend/backend-testing-commons/src/main/java/at/fhj/swd14/pse/person/AbstractPersonInformationDtoTester.java
@@ -4,6 +4,11 @@ import org.junit.Assert;
 
 public class AbstractPersonInformationDtoTester {
 
+	private AbstractPersonInformationDtoTester()
+	{
+		//gets rid of the default constructor
+	}
+	
     public static void assertEquals(AbstractPersonInformationDto expected, AbstractPersonInformationDto actual) {
         Assert.assertEquals(expected.getId(), actual.getId());
         Assert.assertEquals(expected.getValue(), actual.getValue());

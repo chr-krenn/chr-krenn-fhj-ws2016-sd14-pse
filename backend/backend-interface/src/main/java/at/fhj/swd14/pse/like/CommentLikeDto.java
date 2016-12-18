80795c2e8916f1ad18a998c2b657f5aaf3bcbfc3
Richard Raumberger <richard.raumberger@edu.fh-joanneum.at>
2016-12-18 13:50:11 +0100

    remove unneccessary string buffer

--- backend/backend-interface/src/main/java/at/fhj/swd14/pse/like/CommentLikeDto.java
+++ backend/backend-interface/src/main/java/at/fhj/swd14/pse/like/CommentLikeDto.java
@@ -4,63 +4,70 @@ import java.io.Serializable;
 
 import at.fhj.swd14.pse.comment.CommentDto;
 import at.fhj.swd14.pse.user.UserDto;
+
 /**
  * DTO for a like of a comment.
- * @author Thomas
  *
+ * @author Thomas
  */
 public class CommentLikeDto implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	
-	private UserDto liker;
-	private CommentDto likedComment;
-	
-	/**
+    private static final long serialVersionUID = 1L;
+
+    private UserDto liker;
+    private CommentDto likedComment;
+
+    /**
      * Construct a new like for a comment.
-     * @param liker The user, who likes the comment.
+     *
+     * @param liker   The user, who likes the comment.
      * @param comment The comment, which is liked.
      */
-    public CommentLikeDto(UserDto liker,CommentDto comment) {
-    	this.liker = liker;
-    	this.likedComment = comment;
+    public CommentLikeDto(UserDto liker, CommentDto comment) {
+        this.liker = liker;
+        this.likedComment = comment;
     }
+
     /**
      * Sets the person who likes a comment.
+     *
      * @param liker The person who likes a comment.
      */
     public void setLiker(UserDto liker) {
-    	this.liker = liker;
+        this.liker = liker;
     }
+
     /**
      * Returns the liker of a comment.
+     *
      * @return The liker of a comment.
      */
     public UserDto getLiker() {
-    	return this.liker;
+        return this.liker;
     }
+
     /**
      * Sets the liked comment.
+     *
      * @param likedComment The comment to be liked.
      */
     public void setLikedComment(CommentDto likedComment) {
-    	this.likedComment = likedComment;
+        this.likedComment = likedComment;
     }
+
     /**
      * Returns the liked comment.
+     *
      * @return The liked comment.
      */
     public CommentDto getLikedComment() {
-    	return this.likedComment;
+        return this.likedComment;
     }
-    
+
     @Override
     public String toString() {
-    	StringBuffer sb = new StringBuffer();
-    	sb.append("CommentLikeDto");
-    	sb.append("   liker: " + this.liker.getId());
-    	sb.append("   comment: " + this.likedComment.getId());
-    	
-    	return sb.toString();
+        return "CommentLikeDto" +
+                "   liker: " + this.liker.getId() +
+                "   comment: " + this.likedComment.getId();
     }
 }

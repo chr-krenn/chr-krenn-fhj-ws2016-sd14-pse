82944440e261d9b4385ca5d229097c3df8504ea6
Thomas <thomas.klampfl@edu.fh-joanneum.at>
2016-12-19 20:27:52 +0100

    MessageLikeServiceImpl finished.

--- backend/backend-impl/src/main/java/at/fhj/swd14/pse/like/MessageLikeServiceImpl.java
+++ backend/backend-impl/src/main/java/at/fhj/swd14/pse/like/MessageLikeServiceImpl.java
@@ -1,14 +1,17 @@
 package at.fhj.swd14.pse.like;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.EJB;
 
 import at.fhj.swd14.pse.converter.MessageConverter;
 import at.fhj.swd14.pse.converter.UserConverter;
+import at.fhj.swd14.pse.message.Message;
 import at.fhj.swd14.pse.message.MessageDto;
 import at.fhj.swd14.pse.repository.MessageRepository;
 import at.fhj.swd14.pse.repository.UserRepository;
+import at.fhj.swd14.pse.user.User;
 import at.fhj.swd14.pse.user.UserDto;
 
 public class MessageLikeServiceImpl implements MessageLikeService {
@@ -29,26 +32,44 @@ public class MessageLikeServiceImpl implements MessageLikeService {
 
 	@Override
 	public MessageLikeDto getMessageLike(long userId, long messageId) {
-		// TODO Auto-generated method stub
-		return null;
+		MessageDto messageDTO = MessageConverter.convert(messageRepository.find(messageId));
+		UserDto userDTO = UserConverter.convert(userRepository.find(userId));
+		MessageLikeDto messageLike = new MessageLikeDto(userDTO,messageDTO);
+		
+		return messageLike;
 	}
 
 	@Override
 	public List<MessageLikeDto> getMessageLikes(long messageId) {
-		// TODO Auto-generated method stub
-		return null;
+		MessageDto messageDTO = MessageConverter.convert(messageRepository.find(messageId));
+		Message message = messageRepository.find(messageId);
+		List<User> users = message.getUsers();
+		List<MessageLikeDto> messageLikes = new ArrayList<MessageLikeDto>();
+		for (int i = 0; i < users.size(); i++)
+		{
+			UserDto userDTO = UserConverter.convert(users.get(i));
+			MessageLikeDto messageLike = new MessageLikeDto(userDTO,messageDTO);
+			messageLikes.add(messageLike);
+			
+		}
+		
+		return messageLikes;
 	}
 
 	@Override
 	public int getLikeCountForMessage(long messageId) {
-		// TODO Auto-generated method stub
-		return 0;
+		Message message = messageRepository.find(messageId);
+		List<User> users = message.getUsers();
+		
+		return users.size();
 	}
 
 	@Override
 	public int getLikeCountForUserForMessages(long userId) {
-		// TODO Auto-generated method stub
-		return 0;
+		User user = userRepository.find(userId);
+		List<Message> messages = user.getMessages();
+		
+		return messages.size();
 	}
 
 }

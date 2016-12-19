3959fcc512b10e5ff5df8d43929b9a3bf532bb40
Thomas <thomas.klampfl@edu.fh-joanneum.at>
2016-12-19 18:31:53 +0100

    Added implementation classes for likes.

---
+++ backend/backend-impl/src/main/java/at/fhj/swd14/pse/like/CommentLikeServiceImpl.java
@@ -0,0 +1,37 @@
+package at.fhj.swd14.pse.like;
+
+import java.util.List;
+
+public class CommentLikeServiceImpl implements CommentLikeService {
+
+	@Override
+	public void save(CommentLikeDto commentLike) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public CommentLikeDto getCommentLike(long userId, long commentId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CommentLikeDto> getCommentLikes(long commentId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getLikeCountForComment(long commentId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getLikeCountForUserForComments(long userId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ backend/backend-impl/src/main/java/at/fhj/swd14/pse/like/MessageLikeServiceImpl.java
@@ -0,0 +1,37 @@
+package at.fhj.swd14.pse.like;
+
+import java.util.List;
+
+public class MessageLikeServiceImpl implements MessageLikeService {
+
+	@Override
+	public void save(MessageLikeDto messageLike) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public MessageLikeDto getMessageLike(long userId, long messageId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MessageLikeDto> getMessageLikes(long messageId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getLikeCountForMessage(long messageId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getLikeCountForUserForMessages(long userId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

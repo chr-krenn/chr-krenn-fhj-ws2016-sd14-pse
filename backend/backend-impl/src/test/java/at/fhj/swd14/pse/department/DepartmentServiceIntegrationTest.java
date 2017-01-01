1bd320c493a441d90577b7102067e1b47e696958
Richard Raumberger <richard.raumberger@edu.fh-joanneum.at>
2017-01-01 19:18:48 +0100

    refactor integration test framework

---
+++ backend/backend-impl/src/test/java/at/fhj/swd14/pse/department/DepartmentServiceIntegrationTest.java
@@ -0,0 +1,38 @@
+package at.fhj.swd14.pse.department;
+
+import at.fhj.swd14.pse.base.IntegrationTestUtil;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.naming.NamingException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class DepartmentServiceIntegrationTest {
+
+    private DepartmentService service;
+
+    @Before
+    public void setup() throws NamingException {
+        service = IntegrationTestUtil.getService(DepartmentService.class);
+    }
+
+    @Test
+    public void testFindAll() {
+        Collection<DepartmentDto> dtos = service.findAll();
+        Assert.assertNotNull(dtos);
+        Assert.assertEquals(2, dtos.size());
+        List<DepartmentDto> dtoList = new ArrayList<>(dtos);
+        int found = 0;
+        for (DepartmentDto dto : dtoList) {
+            if (dto.getName().equals("IT"))
+                found |= 1;
+            if (dto.getName().equals("Accounting"))
+                found |= 2;
+        }
+        Assert.assertEquals(3, found);
+    }
+
+}

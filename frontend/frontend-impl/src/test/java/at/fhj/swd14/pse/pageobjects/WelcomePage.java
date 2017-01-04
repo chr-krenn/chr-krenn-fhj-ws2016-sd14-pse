78f3c749c869bd8b1202a6a0b2dac97acb71ab63
obendrau14 <stefan.obendrauf@edu.fh-joanneum.at>
2017-01-04 20:06:19 +0100

    CHG: added some IDs for web object for GUI Tests; added Selenium Test for Welcome Page (MessageStream)

--- frontend/frontend-impl/src/test/java/at/fhj/swd14/pse/pageobjects/WelcomePage.java
+++ frontend/frontend-impl/src/test/java/at/fhj/swd14/pse/pageobjects/WelcomePage.java
@@ -2,11 +2,31 @@ package at.fhj.swd14.pse.pageobjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 
 public class WelcomePage extends AbstractPage {
 
 	private final By profileLink = By.linkText("Profile");
 	private final By usersLink = By.linkText("Users");
+	private final By addMessageTitle = By.id("j_addMessage:j_addMessageTitle");
+	private final By addMessageContent = By.id("j_addMessage:j_addMessageContent");
+	private final By addMessageSubmit = By.id("j_addMessage:j_addMessageSubmit");
+	private final By firstMessageTitle = By.id("messagestream_scroller:scroller_message:0:messagetitle");
+	private final By firstMessageContent = By.id("messagestream_scroller:scroller_message:0:messagecontent");
+	private final By firstMessageLikeCount = By.id("messagestream_scroller:scroller_message:0:form_like_message:text_like_message");
+	private final By firstMessageLikeButton = By.id("messagestream_scroller:scroller_message:0:form_like_message:btn_Like_message");
+	private final By firstMessageAddCommentText = By.id("messagestream_scroller:scroller_message:0:form_newcomment:text_newcomment_content");
+	private final By firstMessageAddCommentSubmit = By.id("messagestream_scroller:scroller_message:0:form_newcomment:text_newcomment_submit");
+	private final By firstMessageLoadMoreComments = By.id("messagestream_scroller:scroller_message:0:scroller_comment:scroller_load_more_comments");
+	private final By firstMessageFirstCommentLikeCount = By.id("messagestream_scroller:scroller_message:0:scroller_comment:0:form_like_comment:text_like_comment");
+	private final By firstMessageFirstCommentLikeButton = By.id("messagestream_scroller:scroller_message:0:scroller_comment:0:form_like_comment:submit_like_comment");
+	private final By communitySelect = By.id("j_selectCommunity:select_community");
+	private final By communitySelectPrivate = By.id("j_selectCommunity:select_community_1");
+	private final By communitySelectAll = By.id("j_selectCommunity:select_community_0");
+	
+	private static final String FIRSTMESSAGECOMMENTLISTSTRING = "//div[@id='messagestream_scroller:scroller_message:0:scroller_comment']/div/ul/li";
 
 	public WelcomePage(WebDriver webdriver) {
 		super(webdriver, "welcome");
@@ -20,5 +40,74 @@ public class WelcomePage extends AbstractPage {
     public UsersPage changeToUsersPage() {
     	webdriver.findElement(usersLink).click();
     	return new UsersPage(webdriver);
+    }
+    
+    public void InsertNewMessage(String title, String content){
+    	webdriver.findElement(addMessageTitle).sendKeys(title);
+    	webdriver.findElement(addMessageContent).sendKeys(content);
+    	webdriver.findElement(addMessageSubmit).click();
+    }
+    
+    public String GetFirstMessageTitle(){
+    	return webdriver.findElement(firstMessageTitle).getText();
+    }
+    
+    public String GetFirstMessageContent(){
+    	return webdriver.findElement(firstMessageContent).getText();
+    }
+    
+    public int GetFirstMessageLikeCount(){
+    	return Integer.parseInt(webdriver.findElement(firstMessageLikeCount).getText());
+    }
+    
+    public void ToggleLikeFirstMessage(){
+    	webdriver.findElement(firstMessageLikeButton).click();
+    }
+    
+    public int GetFirstMessageCommentCount(){
+    	return webdriver.findElements(By.xpath(FIRSTMESSAGECOMMENTLISTSTRING)).size();
+    }
+    
+    public void AddCommentToFirstMessage(String content){
+    	webdriver.findElement(firstMessageAddCommentText).sendKeys(content);
+    }
+    
+    public void SubmitCommentToFirstMessage(){
+    	webdriver.findElement(firstMessageAddCommentSubmit).click();
+    }
+    
+    public void FirstMessageLoadMoreComments(){
+    	webdriver.findElement(firstMessageLoadMoreComments).click();
+    	Wait();
+    }
+    
+    public int GetFirstMessageFirstCommentLikeCount(){
+    	return Integer.parseInt(webdriver.findElement(firstMessageFirstCommentLikeCount).getText());
+    }
+    
+    public void ToggleLikeFirstMessageFirstComment(){
+    	webdriver.findElement(firstMessageFirstCommentLikeButton).click();
+    	Wait();
+    }
+    
+    public void CollapseCommunityDropdown(){
+    	webdriver.findElement(communitySelect).click();
+    }
+    
+    public WebElement GetPrivateCommunity(){
+    	return webdriver.findElement(communitySelectPrivate);
+    }
+    
+    public WebElement GetAllCommunity(){
+    	return webdriver.findElement(communitySelectAll);
+    }
+    
+    private void Wait(){
+    	//Sorry for this, but did not found anything to wait for.
+    	try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
     }
 }
---
+++ frontend/frontend-impl/src/test/java/at/fhj/swd14/pse/uitest/MessageStreamUITest.java
@@ -0,0 +1,123 @@
+package at.fhj.swd14.pse.uitest;
+
+import at.fhj.swd14.pse.pageobjects.WelcomePage;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class MessageStreamUITest extends BaseUITest {
+	private final static String TestTitle = "TestMessageTitle";
+	private final static String TestContent = "TestMessageContent";
+	private final static String TestCommentContent = "TestMessageCommentContent";
+	
+    @BeforeClass
+    public static void setup() {
+        login();
+    }
+
+    @Test
+    public void testPostMessage() {
+        WelcomePage welcomePage = getWelcomePage();
+        InsertNewMessage(welcomePage);
+    }
+    
+    @Test
+    public void testLikeMessage() {
+        WelcomePage welcomePage = getWelcomePage();
+        InsertNewMessage(welcomePage);
+        welcomePage.ToggleLikeFirstMessage();
+        Assert.assertEquals(1, welcomePage.GetFirstMessageLikeCount());
+        welcomePage.ToggleLikeFirstMessage();
+        Assert.assertEquals(0, welcomePage.GetFirstMessageLikeCount());
+    }
+    
+    @Test
+    public void testAddCommentToMessage() {
+        WelcomePage welcomePage = getWelcomePage();
+        InsertNewMessage(welcomePage);
+        Assert.assertEquals(0, welcomePage.GetFirstMessageCommentCount());
+        welcomePage.AddCommentToFirstMessage(TestCommentContent);
+        welcomePage.SubmitCommentToFirstMessage();
+        welcomePage = getWelcomePage();
+        Assert.assertEquals(1, welcomePage.GetFirstMessageCommentCount());
+    }
+    
+    @Test
+    public void testLikeCommentOfMessage() {
+        WelcomePage welcomePage = getWelcomePage();
+        InsertNewMessage(welcomePage);
+        Assert.assertEquals(0, welcomePage.GetFirstMessageCommentCount());
+        welcomePage.AddCommentToFirstMessage(TestCommentContent);
+        welcomePage.SubmitCommentToFirstMessage();
+        welcomePage = getWelcomePage();
+        Assert.assertEquals(1, welcomePage.GetFirstMessageCommentCount());
+        
+        Assert.assertEquals(0, welcomePage.GetFirstMessageFirstCommentLikeCount());
+        welcomePage.ToggleLikeFirstMessageFirstComment();
+        Assert.assertEquals(1, welcomePage.GetFirstMessageFirstCommentLikeCount());
+        welcomePage.ToggleLikeFirstMessageFirstComment();
+        Assert.assertEquals(0, welcomePage.GetFirstMessageFirstCommentLikeCount());
+    }
+    
+    @Test
+    public void testAddManyCommentsAndLoadMoreToMessage() {
+        WelcomePage welcomePage = getWelcomePage();
+        InsertNewMessage(welcomePage);
+        Assert.assertEquals(0, welcomePage.GetFirstMessageCommentCount());
+        welcomePage.AddCommentToFirstMessage(TestCommentContent);
+        for (int i = 0; i < 15; i++) {
+        	welcomePage.SubmitCommentToFirstMessage();
+		}
+        welcomePage = getWelcomePage();
+        Assert.assertEquals(10, welcomePage.GetFirstMessageCommentCount());
+        welcomePage.FirstMessageLoadMoreComments();
+        Assert.assertEquals(15, welcomePage.GetFirstMessageCommentCount());
+    }
+    
+    @Test
+    public void testChangeCommunityFilter() {
+        WelcomePage welcomePage = getWelcomePage();
+        InsertNewMessage(welcomePage);
+        welcomePage.CollapseCommunityDropdown();
+        WebElement privateCommunity = welcomePage.GetPrivateCommunity();
+        Assert.assertEquals("Private", privateCommunity.getText());
+        privateCommunity.click();
+        Wait();
+        Assert.assertNotEquals(TestTitle, welcomePage.GetFirstMessageTitle());
+        Assert.assertNotEquals(TestContent, welcomePage.GetFirstMessageContent());
+        welcomePage.CollapseCommunityDropdown();
+        WebElement allCommunity = welcomePage.GetAllCommunity();
+        Assert.assertEquals("Alle", allCommunity.getText());
+        allCommunity.click();
+        Wait();
+        Assert.assertEquals(TestTitle, welcomePage.GetFirstMessageTitle());
+        Assert.assertEquals(TestContent, welcomePage.GetFirstMessageContent());
+    }
+    
+    private WelcomePage getWelcomePage(){
+    	WelcomePage welcomePage = gotoStartPage();
+        boolean isWelcomePage = webdriver.getTitle().toLowerCase().contains("welcome");
+        Assert.assertTrue(isWelcomePage);
+        return welcomePage;
+    }
+    
+    private WelcomePage InsertNewMessage(WelcomePage welcomePage){
+    	welcomePage.InsertNewMessage(TestTitle, TestContent);
+        welcomePage = getWelcomePage();
+        Assert.assertEquals(TestTitle, welcomePage.GetFirstMessageTitle());
+        Assert.assertEquals(TestContent, welcomePage.GetFirstMessageContent());
+        Assert.assertEquals(0, welcomePage.GetFirstMessageLikeCount());
+        return welcomePage;
+    }
+    
+    private void Wait(){
+    	//Sorry for this, but did not found anything to wait for.
+    	try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    }
+}

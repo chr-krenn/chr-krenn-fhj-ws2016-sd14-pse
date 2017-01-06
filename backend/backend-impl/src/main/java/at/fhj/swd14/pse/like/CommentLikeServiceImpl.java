d29cee32ebdaa51f0a2e2f75f3a04a2df492a7bb
Thomas <thomas.klampfl@edu.fh-joanneum.at>
2017-01-06 18:09:54 +0100

    Exception Handling and Logging for CommentLikeService added.

--- backend/backend-impl/src/main/java/at/fhj/swd14/pse/like/CommentLikeServiceImpl.java
+++ backend/backend-impl/src/main/java/at/fhj/swd14/pse/like/CommentLikeServiceImpl.java
@@ -4,6 +4,7 @@ import at.fhj.swd14.pse.comment.Comment;
 import at.fhj.swd14.pse.comment.CommentConverter;
 import at.fhj.swd14.pse.comment.CommentDto;
 import at.fhj.swd14.pse.comment.CommentRepository;
+import at.fhj.swd14.pse.exception.VerificationException;
 import at.fhj.swd14.pse.user.User;
 import at.fhj.swd14.pse.user.UserConverter;
 import at.fhj.swd14.pse.user.UserDto;
@@ -12,12 +13,17 @@ import at.fhj.swd14.pse.user.UserRepository;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Stateless
 public class CommentLikeServiceImpl implements CommentLikeService {
 
+	private static final Logger LOGGER = LogManager.getLogger(CommentLikeServiceImpl.class);
+	
     @EJB
     private CommentRepository commentRepository;
     @EJB
@@ -25,6 +31,12 @@ public class CommentLikeServiceImpl implements CommentLikeService {
 
     @Override
     public void save(CommentLikeDto commentLike) {
+    	LOGGER.trace("Method save in CommentLikeService invoked.");
+    	try {
+    		if (commentLike == null){
+    			LOGGER.error("Can not process comment like NULL");
+        		throw new VerificationException("Can not process NULL as comment like");
+    		}
     	CommentDto commentDTO = commentLike.getLikedComment();
         long commentId = CommentConverter.convert(commentDTO).getId();
         Comment comment = commentRepository.find(commentId);
@@ -54,10 +66,19 @@ public class CommentLikeServiceImpl implements CommentLikeService {
         }
         comment.setUsers(users);
         commentRepository.save(comment);
+    	} catch(VerificationException e){
+    		LOGGER.error("Illegal input from frontend." + e.getMessage(), e);
+			throw new CommentLikeServiceException("Illegal input from frontend." + e.getMessage());
+    	} catch(Exception e){
+    		LOGGER.error("An error occurred while saving comment like.", e);
+			throw new CommentLikeServiceException("Comment like could not be saved.");
+    	}
     }
 
     @Override
     public CommentLikeDto getCommentLike(long userId, long commentId) {
+    	LOGGER.trace("Method getCommentLike invoked");
+    	try {
     	CommentDto commentDTO = CommentConverter.convert(commentRepository.find(commentId));
         Comment comment = commentRepository.find(commentId);
         UserDto userDTO = null;
@@ -79,10 +100,16 @@ public class CommentLikeServiceImpl implements CommentLikeService {
         CommentLikeDto commentLike = new CommentLikeDto(userDTO, commentDTO);
 
         return commentLike;
+    	} catch(Exception e) {
+    		LOGGER.error("An error occured while searching for comment like for: " + userId + " " + commentId, e);
+    		throw new CommentLikeServiceException("Comment like for " + userId + " " + commentId + " could not be retrieved.");
+    	}
     }
 
     @Override
     public List<CommentLikeDto> getCommentLikes(long commentId) {
+    	LOGGER.trace("Method getCommentLikes invoked.");
+    	try {
         CommentDto commentDTO = CommentConverter.convert(commentRepository.find(commentId));
         Comment comment = commentRepository.find(commentId);
         List<User> users = comment.getUsers();
@@ -93,22 +120,38 @@ public class CommentLikeServiceImpl implements CommentLikeService {
         }
 
         return commentLikes;
+    	} catch (Exception e){
+    		LOGGER.error("An error occured while searching for comment likes for: " + commentId, e);
+    		throw new CommentLikeServiceException("Comment likes for " + commentId + " could not be retrieved.");
+    	}
     }
 
     @Override
     public int getLikeCountForComment(long commentId) {
+    	LOGGER.trace("Method getLikeCountForComment invoked.");
+    	try {
         Comment comment = commentRepository.find(commentId);
         List<User> users = comment.getUsers();
 
         return users.size();
+    	} catch (Exception e) {
+    		LOGGER.error("An error occured while searching for like count for comment: " + commentId, e);
+    		throw new CommentLikeServiceException("Like count for comment " + commentId + " could not be retrieved.");
+    	}
     }
 
     @Override
     public int getLikeCountForUserForComments(long userId) {
+    	LOGGER.trace("Method getLikeCountForUserForComments invoked.");
+    	try {
         User user = userRepository.find(userId);
         List<Comment> comments = user.getComments();
 
         return comments.size();
+    	} catch (Exception e) {
+    		LOGGER.error("An error occured while searching for like count for comment for user: " + userId, e);
+    		throw new CommentLikeServiceException("Like count for comments for user: " + userId + " could not be retrieved.");
+    	}
     }
 
 }

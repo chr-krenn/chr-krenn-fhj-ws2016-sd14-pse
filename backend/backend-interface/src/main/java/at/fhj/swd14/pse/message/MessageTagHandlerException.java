87b43f11d5da7b38ed4618b1b55ba1191dc3108c
goellesh14 <student@localhost.localdomain>
2017-01-08 01:22:08 +0100

    Changed exception type

--- backend/backend-interface/src/main/java/at/fhj/swd14/pse/message/MessageTagHandlerException.java
+++ backend/backend-interface/src/main/java/at/fhj/swd14/pse/message/MessageTagHandlerException.java
@@ -1,11 +1,13 @@
 package at.fhj.swd14.pse.message;
 
+import at.fhj.swd14.pse.exception.ServiceException;
+
 /**
  * 
  * @author Gölles-Haas
  *
  */
-public class MessageTagHandlerException extends Exception {
+public class MessageTagHandlerException extends ServiceException {
 
 	private static final long serialVersionUID = 1L;
 	

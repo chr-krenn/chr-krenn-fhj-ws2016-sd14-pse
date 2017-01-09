56ab4c6e3d1a72b46c36da77b2e8ba94f8af169f
Michael König <michael.caspar.koenig@gmail.com>
2017-01-09 23:10:23 +0100

    got rid of old todo- and commit-lines

--- frontend/frontend-impl/src/main/java/at/fhj/swd14/pse/message/MessageBean.java
+++ frontend/frontend-impl/src/main/java/at/fhj/swd14/pse/message/MessageBean.java
@@ -40,10 +40,16 @@ public class MessageBean implements Serializable {
      * The user that posts the message
      */
     private UserDto currentUser;
+    
+    /**
+     * The recipient of the message
+     */
+    private UserDto recipient;
 	
     public MessageBean() {
     	LOGGER.debug("Create: " + MessageBean.class.getSimpleName());
     	this.message = new MessageDto();
+    	this.recipient = new UserDto();
     }
     
     public MessageDto getMessage() {
@@ -55,7 +61,6 @@ public class MessageBean implements Serializable {
      */
     public void createMessage() {
     	LOGGER.info("Creating message");
-        // TODO: add author and community/recipient
     	if(currentUser == null) {
             long userID =  ((at.fhj.swd14.pse.security.DatabasePrincipal) FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal()).getUserId();
             currentUser = userService.find(userID); 
@@ -64,15 +69,16 @@ public class MessageBean implements Serializable {
     	if(currentUser != null) {
     		if(message.getContent() != null && message.getTitle() != null) {
     			message.setAuthor(currentUser);
-    			// TODO: insert accurate community - may stay null, if not a local message!
     			
-    			
-    			// TODO: insert accurate recipient - may stay null, if not a private message!
+    			// Field recipient shall be set from outside
+    			if(recipient.getId() != null) {
+    				message.setRecipient(recipient);
+    			}    			
 
     	    	final long generatedId = messageService.save(message);
     	    	LOGGER.info("Created new message with ID {}", generatedId);
-    	    	//this.message = messageService.find(generatedId);
     	    	this.message = new MessageDto();
+    	    	this.recipient = new UserDto();
     		}
     	
     	} else {

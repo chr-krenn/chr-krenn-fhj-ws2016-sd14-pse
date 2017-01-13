008449045635f641eb1b47542715e1b2949061dd
michael-mammut <michael.mammut@gmail.com>
2017-01-13 18:33:57 +0100

    Add- CommunityTest - findAll

--- backend/backend-impl/src/test/java/at/fhj/swd14/pse/community/CommunityTest.java
+++ backend/backend-impl/src/test/java/at/fhj/swd14/pse/community/CommunityTest.java
@@ -1,7 +1,25 @@
 package at.fhj.swd14.pse.community;
 
-import org.junit.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import at.fhj.swd14.pse.user.User;
+import at.fhj.swd14.pse.user.UserDto;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CommunityTest {
 
     @BeforeClass
@@ -18,7 +36,51 @@ public class CommunityTest {
     public void insertCommunity() {
         //test
     }
+    
+    
+    
+    @Mock
+    private CommunityRepository communityRepository;
+    
+    @InjectMocks
+    private CommunityServiceImpl communityService;
+    
+    @Test 
+    public void findAllCommunities(){
+    	 when(communityRepository.findAll()).thenReturn(Arrays.asList(buildCommunities(), buildCommunities(), buildCommunities()));
+         final Collection<CommunityDto> coms = communityService.findAll();
 
+         assertEquals(3, coms.size());
+         coms.forEach(newsDto -> assertEquals(buildCommunityDtos(), newsDto));
+    }
+
+    private Community buildCommunities(){
+    	 final Community com = new Community();
+    	 com.setActiveState(true);
+    	 com.setAllowedUsers(Arrays.asList(new User()));
+    	 com.setAllowedUsersInactive(new User());
+    	 com.setAuthor(new User());
+    	 com.setId(1L);
+    	 com.setName("TEST");
+    	 com.setPublicState(true);
+    	 com.setUserCommunities(Arrays.asList(new UserCommunity()));
+    	 
+    	 return com;
+    }
+    
+    private CommunityDto buildCommunityDtos(){
+    	final CommunityDto com = new CommunityDto();
+    	com.setActiveState(true);
+   	 com.setAllowedUsers(Arrays.asList(new UserDto()));
+   	 com.setAuthor(new UserDto());
+   	 com.setId(1L);
+   	 com.setName("TEST");
+   	 com.setPublicState(true);
+   	 
+   	 return com;
+    }
+    
+    
     @After
     public void teardown() {
         //drop

b36aa769c7dc4b9ebfbac0fb5e45785df56be2a1
purkart <julianpurkart@gmail.com>
2017-01-14 18:08:58 +0100

    readded private constructor

--- backend/backend-impl/src/main/java/at/fhj/swd14/pse/community/CommunityConverter.java
+++ backend/backend-impl/src/main/java/at/fhj/swd14/pse/community/CommunityConverter.java
@@ -8,52 +8,55 @@ import java.util.stream.Collectors;
 
 public final class CommunityConverter {
 
-    public static CommunityDto convert(Community community) {
-        if (community == null) {
-            return null;
-        }
-        CommunityDto dto = new CommunityDto(community.getId());
-        dto.setId(community.getId());
-        dto.setAllowedUsers(UserConverter.convertToDtoList(community.getAllowedUsers()));
-        dto.setPendingUsers(UserConverter.convertToDtoList(community.getPendingUsers()));
+	private CommunityConverter() {
 
-        dto.setName(community.getName());
-        dto.setAuthor(UserConverter.convert(community.getAuthor()));
-        dto.setActiveState(community.geActiveState());
-        dto.setPublicState(community.getPublicState());
+	}
 
-        return dto;
-    }
+	public static CommunityDto convert(Community community) {
+		if (community == null) {
+			return null;
+		}
+		CommunityDto dto = new CommunityDto(community.getId());
+		dto.setId(community.getId());
+		dto.setAllowedUsers(UserConverter.convertToDtoList(community.getAllowedUsers()));
+		dto.setPendingUsers(UserConverter.convertToDtoList(community.getPendingUsers()));
 
-    public static Community convert(CommunityDto dto) {
-        if (dto == null) {
-            return null;
-        }
-        Community community = new Community();
-        community.setId(dto.getId());
-        community.setAllowedUsers(UserConverter.convertToDoList( dto.getAllowedUsers()));
-        community.setPendingUsers(UserConverter.convertToDoList( dto.getPendingUsers()));
+		dto.setName(community.getName());
+		dto.setAuthor(UserConverter.convert(community.getAuthor()));
+		dto.setActiveState(community.geActiveState());
+		dto.setPublicState(community.getPublicState());
 
-        community.setName(dto.getName());
-        community.setAuthor(UserConverter.convert(dto.getAuthor()));
-        community.setActiveState(dto.getActiveState());
-        community.setPublicState(dto.getPublicState());
+		return dto;
+	}
 
+	public static Community convert(CommunityDto dto) {
+		if (dto == null) {
+			return null;
+		}
+		Community community = new Community();
+		community.setId(dto.getId());
+		community.setAllowedUsers(UserConverter.convertToDoList(dto.getAllowedUsers()));
+		community.setPendingUsers(UserConverter.convertToDoList(dto.getPendingUsers()));
 
-        return community;
-    }
+		community.setName(dto.getName());
+		community.setAuthor(UserConverter.convert(dto.getAuthor()));
+		community.setActiveState(dto.getActiveState());
+		community.setPublicState(dto.getPublicState());
 
-    public static List<CommunityDto> convertToDtoList(Collection<Community> communities) {
-        if (communities == null) {
-            return null;
-        }
-        return communities.stream().map(CommunityConverter::convert).collect(Collectors.toList());
-    }
+		return community;
+	}
 
-    public static List<Community> convertToDoList(Collection<CommunityDto> communities) {
-        if (communities == null) {
-            return null;
-        }
-        return communities.stream().map(CommunityConverter::convert).collect(Collectors.toList());
-    }
+	public static List<CommunityDto> convertToDtoList(Collection<Community> communities) {
+		if (communities == null) {
+			return null;
+		}
+		return communities.stream().map(CommunityConverter::convert).collect(Collectors.toList());
+	}
+
+	public static List<Community> convertToDoList(Collection<CommunityDto> communities) {
+		if (communities == null) {
+			return null;
+		}
+		return communities.stream().map(CommunityConverter::convert).collect(Collectors.toList());
+	}
 }

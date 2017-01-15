519977160322a4873c615cc404be74dd6bc706b2
Richard Raumberger <richard.raumberger@gmail.com>
2017-01-15 12:59:20 +0100

    prepare for 1.2 iteration

--- backend/backend-testing-commons/src/main/java/at/fhj/swd14/pse/base/IntegrationTestUtil.java
+++ backend/backend-testing-commons/src/main/java/at/fhj/swd14/pse/base/IntegrationTestUtil.java
@@ -13,8 +13,8 @@ import java.util.Properties;
 
 public final class IntegrationTestUtil {
 
-    private static final String JNDI_APP_NAME = "backend-assembly-1.1";
-    private static final String JNDI_MODULE_NAME = "backend-impl-1.1";
+    private static final String JNDI_APP_NAME = "backend-assembly-1.2-SNAPSHOT";
+    private static final String JNDI_MODULE_NAME = "backend-impl-1.2-SNAPSHOT";
     private static InitialContext context;
 
     private IntegrationTestUtil() {
